3bdef0e38dd5dcbc93f919d96d45a01afb67f372
Pilot-Khadka <46417154+Pilot-Khadka@users.noreply.github.com>
2023-11-06 09:35:26 +0545

    added architecture vocabulary

---
+++ parameters.py
@@ -0,0 +1,8 @@
+
+###***************** Search Space Design ***************###
+nodes = [8, 16, 32, 64, 128, 256, 512]
+activations = ['sigmoid', 'tanh', 'relu']
+
+
+###***************** Dataset ***************###
+target_classes = 3
